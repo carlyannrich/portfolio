783bda4431f24cf09ca5750cd565e4be14fb88fd
Carly Richardson <63616608+carlyannrich@users.noreply.github.com>
2021-02-02 23:27:18 +0000

    added a11y add on and created some components

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "../components/Button";
+
+function Home() {
+  return (
+    <div className="Home">
+      <Button />
+    </div>
+  );
+}
+
+export default Home;
